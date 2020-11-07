85b0e3a15cc7341eb859c554a971d133dd7d872c
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-11-08 01:15:38 +0300

    toggle created

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes: Quote[] = [
+    new Quote(1, 'John Lennon', 'Life is what happens when you are busy making other plans',new Date(2019,9,14)),
+    new Quote(2,'Walt Disney','The way to get started is to quit talking and begin doing.',new Date(2019,6,9)),
+    new Quote(3,'Eleanor Roosevelt','If life were predictable it would cease to be life, and be without flavor',new Date(2019,1,12)),
+    new Quote(4,'Nelson Mandela','The greatest glory in living lies not in never falling, but in rising every time we fall.',new Date(2019,11,18)),
+    new Quote(5,'Eleanor Roosevelt','The future belongs to those who believe in the beauty of their dreams',new Date(2019,2,14)),
+    new Quote(6,'Aristotle','It is during our darkest moments that we must focus to see the light.',new Date(2019,3,14)),
+  ];
+
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+
+  toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
+
+
+deleteQuote(isComplete, index){
+  if (isComplete) {
+    let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+
+    if (toDelete){
+      this.quotes.splice(index,1)
+    }
+  }
+}
 
   constructor() { }
 
