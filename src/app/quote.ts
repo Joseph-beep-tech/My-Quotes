89d224b4bf822f40de884061992d28049a0f1962
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-11-09 14:26:39 +0300

    day count created

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,7 @@ export class Quote {
     showDescription: boolean;
     upvote: number;
     downvote: number;
-    constructor(public id: number,public name: string,public description: string, public completeDate: Date){
+    constructor(public id: number,public name: string,public title: string,public description: string, public completeDate: Date){
       this.showDescription=false;
       this.upvote = 0;
       this.downvote = 0;
